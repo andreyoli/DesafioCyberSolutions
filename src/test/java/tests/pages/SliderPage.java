9c7f8183be73a171942842d63a6393ad31041e21
Andrey <andrey.o.paula@gmail.com>
2021-06-27 00:23:12 -0300

    Create SliderPage

---
+++ src/test/java/tests/pages/SliderPage.java
@@ -0,0 +1,42 @@
+package tests.pages;
+
+import core.Dsl;
+import org.openqa.selenium.By;
+
+public class SliderPage {
+    public final Dsl browser = new Dsl();
+
+    /**
+     * Open Register page
+     */
+    public void openSliderPage() {
+        browser.goTo("http://demo.automationtesting.in/Slider.html");
+    }
+
+    /**
+     * Close Browser
+     */
+    public void closeBrowser() {
+        browser.kill();
+    }
+
+    /**
+     * Move slider by percentage
+     * @param percentage percentage to move slider
+     */
+    public void moveSlider(Integer percentage){
+        By slider = new By.ByXPath("//div[@id='slider']/a");
+
+        browser.dragAndDrop(slider, percentage);
+    }
+
+    /**
+     * Get styles in slider
+     * @return return styles in string
+     */
+    public String getSliderStyles() {
+        By slider = new By.ByXPath("//div[@id='slider']/a");
+
+        return browser.getStyleFromElement(slider);
+    }
+}
